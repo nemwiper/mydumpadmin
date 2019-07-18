08200d0f3a5849d8b9d75ca3146131b0428fbdaa
Mario <nemwiper@tiscalinet.it>
2019-07-18 16:58:17 +0200

    Added rotating backups and rsync

--- mysql-dump.sh
+++ mysql-dump.sh
@@ -14,40 +14,38 @@ CONFIGFILE=/etc/mydumpadmin/settings.conf
 source $CONFIGFILE
 TIME_FORMAT='%d%m%Y-%H%M'
 cTime=$(date +"${TIME_FORMAT}")
-LOGFILENAME=$LOG_PATH/mydumpadmin-${cTime}.txt
+LOGFILENAME=$LOG_PATH/mydumpadmin.log
 CREDENTIALS="--defaults-extra-file=$CREDENTIAL_FILE"
 
-
 [ ! -d $LOG_PATH ] && ${MKDIR} -p ${LOG_PATH}
-echo "" > ${LOGFILENAME}
-echo "<<<<<<   Database Dump Report :: `date +%D`  >>>>>>" >> ${LOGFILENAME}
-echo "" >> ${LOGFILENAME}
-echo "DB Name  :: DB Size   Filename" >> ${LOGFILENAME}
+echo "" >${LOGFILENAME}
+echo "<<<<<<   Database Dump Report :: $(date +%D)  >>>>>>" >>${LOGFILENAME}
+echo "" >>${LOGFILENAME}
+echo "DB Name  :: DB Size   Filename" >>${LOGFILENAME}
 
 ### Make a backup ###
-check_config(){
+check_config() {
         [ ! -f $CONFIGFILE ] && close_on_error "Config file not found, make sure config file is correct"
 }
-db_backup(){
+db_backup() {
 
         if [ "$DB_NAMES" == "ALL" ]; then
-		DATABASES=`$MYSQL $CREDENTIALS -h $MYSQL_HOST -P $MYSQL_PORT -Bse 'show databases' | grep -Ev "^(Database|mysql|performance_schema|information_schema)"$`
+                DATABASES=$($MYSQL $CREDENTIALS -h $MYSQL_HOST -P $MYSQL_PORT -Bse 'show databases' | grep -Ev "^(Database|mysql|performance_schema|information_schema)"$)
         else
-		DATABASES=$DB_NAMES
+                DATABASES=$DB_NAMES
         fi
 
         db=""
         [ ! -d $BACKUPDIR ] && ${MKDIR} -p $BACKUPDIR
-                [ $VERBOSE -eq 1 ] && echo "*** Dumping MySQL Database ***"
-                mkdir -p ${LOCAL_BACKUP_DIR}/${cTime}
-        for db in $DATABASES
-        do
+        [ $VERBOSE -eq 1 ] && echo "*** Dumping MySQL Database ***"
+        mkdir -p ${LOCAL_BACKUP_DIR}
+        for db in $DATABASES; do
                 FILE_NAME="${db}.${cTime}.gz"
-                FILE_PATH="${LOCAL_BACKUP_DIR}/${cTime}/"
+                FILE_PATH="${LOCAL_BACKUP_DIR}/"
                 FILENAMEPATH="$FILE_PATH$FILE_NAME"
                 [ $VERBOSE -eq 1 ] && echo -en "Database> $db... \n"
-                ${MYSQLDUMP} ${CREDENTIALS} -h ${MYSQL_HOST} -P $MYSQL_PORT $db | ${GZIP} -9 > $FILENAMEPATH
-                echo "$db   :: `du -sh ${FILENAMEPATH}`"  >> ${LOGFILENAME}
+                ${MYSQLDUMP} ${CREDENTIALS} -h ${MYSQL_HOST} -P $MYSQL_PORT $db | ${GZIP} -9 >$FILENAMEPATH
+                echo "$db   :: $(du -sh ${FILENAMEPATH})" >>${LOGFILENAME}
                 [ $FTP_ENABLE -eq 1 ] && ftp_backup
                 [ $SFTP_ENABLE -eq 1 ] && sftp_backup
                 [ $S3_ENABLE -eq 1 ] && s3_backup
@@ -57,13 +55,13 @@ db_backup(){
 }
 
 ### close_on_error on demand with message ###
-close_on_error(){
+close_on_error() {
         echo "$@"
         exit 99
 }
 
 ### Make sure bins exists.. else close_on_error
-check_cmds(){
+check_cmds() {
         [ ! -x $GZIP ] && close_on_error "FILENAME $GZIP does not exists. Make sure correct path is set in $CONFIGFILE."
         [ ! -x $MYSQL ] && close_on_error "FILENAME $MYSQL does not exists. Make sure correct path is set in $CONFIGFILE."
         [ ! -x $MYSQLDUMP ] && close_on_error "FILENAME $MYSQLDUMP does not exists. Make sure correct path is set in $CONFIGFILE."
@@ -71,26 +69,28 @@ check_cmds(){
         [ ! -x $MKDIR ] && close_on_error "FILENAME $MKDIR does not exists. Make sure correct path is set in $CONFIGFILE."
         [ ! -x $MYSQLADMIN ] && close_on_error "FILENAME $MYSQLADMIN does not exists. Make sure correct path is set in $CONFIGFILE."
         [ ! -x $GREP ] && close_on_error "FILENAME $GREP does not exists. Make sure correct path is set in $CONFIGFILE."
-	if [ $S3_ENABLE -eq 1 ]; then
-	        [ ! -x $S3CMD ] && close_on_error "FILENAME $S3CMD does not exists. Make sure correct path is set in $CONFIGFILE."
-	fi
-	if [ $SFTP_ENABLE -eq 1 ]; then
-		[ ! -x $SCP ] && close_on_error "FILENAME $SCP does not exists. Make sure correct path is set in $CONFIGFILE."
-	fi
+        if [ $S3_ENABLE -eq 1 ]; then
+                [ ! -x $S3CMD ] && close_on_error "FILENAME $S3CMD does not exists. Make sure correct path is set in $CONFIGFILE."
+        fi
+        if [ $SFTP_ENABLE -eq 1 ]; then
+                [ ! -x $SCP ] && close_on_error "FILENAME $SCP does not exists. Make sure correct path is set in $CONFIGFILE."
+        fi
 }
 
 ### Make sure we can connect to server ...
-check_mysql_connection(){
-        ${MYSQLADMIN} ${CREDENTIALS} -h ${MYSQL_HOST} -P ${MYSQL_PORT} ping | ${GREP} 'alive'>/dev/null
+check_mysql_connection() {
+        ${MYSQLADMIN} ${CREDENTIALS} -h ${MYSQL_HOST} -P ${MYSQL_PORT} ping | ${GREP} 'alive' >/dev/null
         [ $? -eq 0 ] || close_on_error "Error: Cannot connect to MySQL Server. Make sure username and password setup correctly in $CONFIGFILE"
 }
 
+delete_old_backups() {
+        # Delete all backups older than X days
+        ${FIND} ${LOCAL_BACKUP_DIR} -mtime +$DELETE_DAYS -exec rm -f {} \;
+}
 
-
-
-ftp_backup(){
-[ $VERBOSE -eq 1 ] && echo "Uploading backup file to FTP"
-ftp -n $FTP_SERVER << EndFTP
+ftp_backup() {
+        [ $VERBOSE -eq 1 ] && echo "Uploading backup file to FTP"
+        ftp -n $FTP_SERVER <<EndFTP
 user "$FTP_USERNAME" "$FTP_PASSWORD"
 binary
 hash
@@ -101,25 +101,26 @@ bye
 EndFTP
 }
 
-sftp_backup(){
+sftp_backup() {
 
         [ $VERBOSE -eq 1 ] && echo "Uploading backup file to SFTP"
         cd ${FILE_PATH}
-        ${SCP} -P ${SFTP_PORT}  "$FILE_NAME" ${SFTP_USERNAME}@${SFTP_HOST}:${SFTP_UPLOAD_DIR}/
-
+        ${SCP} -P ${SFTP_PORT} "$FILE_NAME" ${SFTP_USERNAME}@${SFTP_HOST}:${SFTP_UPLOAD_DIR}/
 }
 
-s3_backup(){
-	[ $VERBOSE -eq 1 ] && echo "Uploading backup file to S3 Bucket"
-	cd ${FILE_PATH}
-	$S3CMD --access_key="$AWS_ACCESS_KEY" --secret_key="$AWS_SECRET_ACCESS_KEY" put "$FILE_NAME" s3://${S3_BUCKET_NAME}/${S3_UPLOAD_LOCATION}/
+rsync() {
+        ${RSYNC} --delete --progress -avz ${LOCAL_BACKUP_DIR} ${RSYNC_USERNAME}@${RSYNC_HOST}:${RSYNC_UPLOAD_DIR}
 }
 
+s3_backup() {
+        [ $VERBOSE -eq 1 ] && echo "Uploading backup file to S3 Bucket"
+        cd ${FILE_PATH}
+        $S3CMD --access_key="$AWS_ACCESS_KEY" --secret_key="$AWS_SECRET_ACCESS_KEY" put "$FILE_NAME" s3://${S3_BUCKET_NAME}/${S3_UPLOAD_LOCATION}/
+}
 
-send_report(){
-        if [ $SENDEMAIL -eq 1 ]
-        then
-                cat ${LOGFILENAME} | mail -vs "Database dump report for `date +%D`" ${EMAILTO}
+send_report() {
+        if [ $SENDEMAIL -eq 1 ]; then
+                cat ${LOGFILENAME} | mail -vs "Database dump report for $(date +%D)" ${EMAILTO}
         fi
 }
 
@@ -129,3 +130,5 @@ check_cmds
 check_mysql_connection
 db_backup
 send_report
+[ $DELETE_ENABLE -eq 1 ] && delete_old_backups
+[ $RSYNC_ENABLE -eq 1 ] && rsync
